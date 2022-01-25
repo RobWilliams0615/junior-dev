4b6d9c62115a9f13a3245c6f0433cf03b3c8e8fa
RicoWilliams <ricowilliams@protonmail.com>
2022-01-24 19:34:29 -0700

    JobListings componenet created

--- junior-dev/src/App.js
+++ junior-dev/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/NavBAr';
+import JobListings from './components/JobListings';
 
 function App() {
   return (
     <div className="App">
       <div className="navbar">
         <Navbar />
+        <JobListings />
       </div>
     </div>
   );
---
+++ junior-dev/src/components/JobListings.js
@@ -0,0 +1,9 @@
+function JobListings() {
+  return (
+    <div className="job-listings">
+      <h1>Listings will go here</h1>
+    </div>
+  );
+}
+
+export default JobListings;
