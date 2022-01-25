28f79b5bff151cd6636930c02d1a855bdf756060
RicoWilliams <ricowilliams@protonmail.com>
2022-01-24 19:29:06 -0700

    navbar componeent created

---
+++ junior-dev/src/components/NavBAr.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="nav-bar">
+      <a href="./">Home</a>
+      <a href="./Jobs">Jobs</a>
+      <a href="./create-post">PostJob</a>
+    </div>
+  );
+}
+
+export default Navbar;
